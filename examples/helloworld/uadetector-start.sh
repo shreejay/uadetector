d45d7cfa5d1ddcb3fb02c16d995e768ce4b0701a
Shreejay Nair <shreejay@users.noreply.github.com>
2015-10-12 11:50:56 -0700

    Stop options
    Updated stop options to stop server properly.

--- examples/helloworld/uadetector-start.sh
+++ examples/helloworld/uadetector-start.sh
@@ -10,14 +10,13 @@ JETTY_RUNNER="target/dependency/jetty-runner.jar"
 
 WAR_FILE="target/uadetector-hello-world*.war"   
 
-JAVA_OPTIONS="-Xms256m -Xmx256m -DSTOP.PORT=8079 -DSTOP.KEY=mustard 
--Djetty.port=80   
+JAVA_OPTIONS="-Xms256m -Xmx256m -DSTOP.PORT=8079   
 -Xloggc:/var/log/uadetector_GC.log -XX:+PrintGCTimeStamps -XX:+PrintGCDetails   
 -XX:+PrintTenuringDistribution
 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log 
 -XX:SurvivorRatio=3   
 -jar"   
-  
+STOP_OPTIONS="--stop-port 8079 --stop-key releasethekraken"
 STDERR_LOG_FILE="/var/log/uadetector_stderr.log"
 STDOUT_LOG_FILE="/var/log/uadetector_stdout.log"
   
@@ -27,13 +26,13 @@ case $1 in
 	start)
 	echo "Start uadetector" 
 	cd $UADETECTOR_DIR  
-	$JAVA $JAVA_OPTIONS $JETTY_RUNNER $WAR_FILE 1>> $STDOUT_LOG_FILE 2>> $STDERR_LOG_FILE &  
+	$JAVA $JAVA_OPTIONS $JETTY_RUNNER $WAR_FILE $STOP_OPTIONS 1>> $STDOUT_LOG_FILE 2>> $STDERR_LOG_FILE &  
 	
 	;;
 	stop) 
 	echo "Stopping Solr"  
 	cd $UADETECTOR_DIR  
-	$JAVA $JETTY_RUNNER $WAR_FILE --stop
+	$JAVA $JETTY_RUNNER $WAR_FILE $STOP_OPTIONS --stop
 	;;
 	restart)  
 	$0 stop   
